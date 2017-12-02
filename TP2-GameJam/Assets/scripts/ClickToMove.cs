6ec267d5b97aaa9af4592c4b350594115a6e4d61
DouvryValentin <valentin.jabba@gmail.com>
2017-12-02 12:19:46 -0500

    Rotation peronnage vers click / Animation debut

--- TP2-GameJam/Assets/scripts/ClickToMove.cs
+++ TP2-GameJam/Assets/scripts/ClickToMove.cs
@@ -4,28 +4,36 @@ using UnityEngine;
 
 public class ClickToMove : MonoBehaviour {
 
-
-    public float vitesse = 10;
-
+    
+    public float vitesse = 5;
+    
     private Vector3 positionCible;
     private bool enMouvement;
+    
 
 	void Start () {
+        
+        
 		positionCible = transform.position;
+        
 		enMouvement = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButton (0)) {
+
+        
+
+        if (Input.GetMouseButton (0)) {
 			if (enMouvement == false)
 				enMouvement = true;
 
 
 			else if(enMouvement)//cancel le movement deja en cours
 				positionCible = transform.position;
-			
-			positionCible = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+                
+
+            positionCible = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			positionCible.z = transform.position.z;
 		}
 		if (enMouvement)
@@ -33,8 +41,14 @@ public class ClickToMove : MonoBehaviour {
 			
 		if (positionCible == transform.position) 
 			enMouvement = false;
-		
-		
-	}
+
+
+        float AngleRad = Mathf.Atan2(positionCible.y - this.transform.position.y, positionCible.x - this.transform.position.x);
+        float AngleDeg = (180 / Mathf.PI) * AngleRad;
+        this.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+
+    }
+
+    
 
 }
