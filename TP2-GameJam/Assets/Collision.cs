4dbb22dc99bcb94d4475df901b10de15315b0a5e
Mezeeh <mezeeh@hotmail.com>
2017-12-02 16:38:56 -0500

    Gerer rammasser la rondelle
    Rondelle se rammasse et se dirige vers la palette

---
+++ TP2-GameJam/Assets/Collision.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour {
+
+	bool joueurControle = false;
+	Vector3 joueurControleStartLocalPos = Vector3.zero;
+	Vector3 joueurPalettePos;
+	float joueurPaletteProgression;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(joueurControle)
+		{
+			joueurPaletteProgression += Time.deltaTime / 0.5f;
+			Debug.Log (joueurPaletteProgression);
+			transform.localPosition = Vector3.Lerp(joueurControleStartLocalPos, joueurPalettePos, joueurPaletteProgression);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if(!joueurControle && coll.transform.tag == "Hero")
+		{
+			joueurPaletteProgression = 0;
+			this.transform.parent = coll.transform;
+			joueurControle = true;
+			joueurControleStartLocalPos = transform.localPosition;
+			joueurPalettePos = transform.parent.Find ("Palette").transform.localPosition;
+		}
+	}
+}
