24436fadfc0b7946a677ba140819a80e67ddab3e
DouvryValentin <valentin.jabba@gmail.com>
2017-12-02 13:00:30 -0500

    123

--- TP2-GameJam/Assets/scripts/OptionMenu.cs
+++ TP2-GameJam/Assets/scripts/OptionMenu.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class OptionMenu : MonoBehaviour {
 
-	public GameObject mainMenu,
-				menuCommentJouer,
-				menuTouches;
-
 	public void quitter()
 	{
 		Debug.Log ("Le jeu va se fermer!");
@@ -16,27 +12,16 @@ public class OptionMenu : MonoBehaviour {
 
 	public void commentJouer()
 	{
-		//GetComponent ("Main Menu").gameObject.SetActive = false;
-		menuCommentJouer.SetActive (true);
-		mainMenu.SetActive (false);
+		
 	}
 
 	public void touches()
 	{
-		menuTouches.SetActive (true);
-		mainMenu.SetActive (false);
+		
 	}
 
 	public void jouer()
 	{
-		mainMenu.SetActive (false);
-		menuCommentJouer.SetActive (false);
-		menuTouches.SetActive (false);
-	}
-
-	public void retour()
-	{
-		mainMenu.SetActive (true);
-		menuCommentJouer.SetActive (false);
+		
 	}
 }
