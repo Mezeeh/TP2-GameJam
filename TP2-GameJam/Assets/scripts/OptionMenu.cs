70e229bcccc6650a040417bc51a2df8282861a06
Mezeeh <mezeeh@hotmail.com>
2017-12-02 12:47:29 -0500

    Menus principal gere
    Menus fonctionnels

--- TP2-GameJam/Assets/scripts/OptionMenu.cs
+++ TP2-GameJam/Assets/scripts/OptionMenu.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class OptionMenu : MonoBehaviour {
 
+	public GameObject mainMenu,
+				menuCommentJouer,
+				menuTouches;
+
 	public void quitter()
 	{
 		Debug.Log ("Le jeu va se fermer!");
@@ -12,16 +16,27 @@ public class OptionMenu : MonoBehaviour {
 
 	public void commentJouer()
 	{
-		
+		//GetComponent ("Main Menu").gameObject.SetActive = false;
+		menuCommentJouer.SetActive (true);
+		mainMenu.SetActive (false);
 	}
 
 	public void touches()
 	{
-		
+		menuTouches.SetActive (true);
+		mainMenu.SetActive (false);
 	}
 
 	public void jouer()
 	{
-		
+		mainMenu.SetActive (false);
+		menuCommentJouer.SetActive (false);
+		menuTouches.SetActive (false);
+	}
+
+	public void retour()
+	{
+		mainMenu.SetActive (true);
+		menuCommentJouer.SetActive (false);
 	}
 }
