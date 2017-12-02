bf836cd431dc8fe862f665224dcf7252d2d421e9
DouvryValentin <valentin.jabba@gmail.com>
2017-12-02 16:24:37 -0500

    GameManager et BoardManager
    Création et écriture des scripts de génération aléatoire des levels

---
+++ TP2-GameJam/Assets/scripts/BoardManager.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
+
+
+public class BoardManager : MonoBehaviour {
+
+    [Serializable]
+
+    public class Count
+    {
+        public int maximum;
+        public int minimum;
+
+        public Count (int min, int max)
+        {
+            minimum = min;
+            maximum = max;
+        }
+    }
+
+    public int columns = 4;
+    public int rows = 8;
+    public Count coneCount = new Count(3, 9);
+
+    public Count adversairesCount = new Count(1, 4);
+    public GameObject terrain;
+    public GameObject but;
+    public GameObject[] cone;
+    public GameObject[] adversaires;
+    //public GameObject[] floorTitles;
+    //public GameObject[] murDeCone;
+    //public GameObject[] outerWallTitles;
+
+    private Transform boardHolder;
+    private List<Vector3> gridPositions = new List<Vector3>();
+
+    void InitialiseList()
+    {
+        gridPositions.Clear();
+
+        for(int x = 1; x < columns -1; x++)
+        {
+            for(int y = 1; y < rows - 1; y++)
+            {
+                gridPositions.Add(new Vector3(x, y, 0f));
+            }
+        }
+    }
+
+    void BoardSetup()
+    {
+        boardHolder = new GameObject("Board").transform;        
+        GameObject instance = Instantiate(terrain, new Vector3(0, 0, 0f), Quaternion.identity) as GameObject;
+        instance.transform.SetParent(boardHolder);
+
+        /*for(int x = -1; x < columns + 1; x++)
+        {
+            for(int y = -1; y < rows + 1; y++)
+            {
+               // GameObject toInstantiate = floorTitles[Random.Range(0, floorTitles.Length)];
+                if (x == -1 || x == columns || y == -1 || y == rows)
+                {
+                    //toInstantiate = outerWallTitles[Random.Range(0, outerWallTitles.Length)];
+                }
+
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x,y,0f),Quaternion.identity) as GameObject;
+                instance.transform.SetParent(boardHolder);
+
+            }
+        }*/
+
+
+    }
+
+    Vector3 RandomPosition()
+    {
+        int randomIndex = Random.Range(0, gridPositions.Count);
+        Vector3 randomPosition = gridPositions[randomIndex];
+        gridPositions.RemoveAt(randomIndex);
+        return randomPosition;
+
+
+    }
+
+    void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum)
+    {
+        int objectCount = Random.Range(minimum, maximum + 1);
+
+        for(int i = 0; i < objectCount; i++)
+        {
+            Vector3 randomPosition = RandomPosition();
+            GameObject tileChoise = tileArray[Random.Range(0, tileArray.Length)];
+            Instantiate(tileChoise, randomPosition, Quaternion.identity);
+        }
+    }
+
+    public void SetupScene(int level)
+    {
+        BoardSetup();
+        InitialiseList();
+        LayoutObjectAtRandom(cone, coneCount.minimum, coneCount.maximum);
+        LayoutObjectAtRandom(adversaires, adversairesCount.minimum, adversairesCount.maximum);
+        Instantiate(but, new Vector3(4.377f,-0.036f,0f), Quaternion.identity);
+    }
+
+
+        
+
+
+
+
+
+	
+}
