2c9653ef3ebeb22ec178cd8e479c7eb2a477ee63
Mezeeh <mezeeh@hotmail.com>
2017-12-02 12:13:17 -0500

    Ajout du Main menu
    ajout de limage ainsi que les boutons et les fonctions sur eux a coder

---
+++ TP2-GameJam/Assets/scripts/OptionMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionMenu : MonoBehaviour {
+
+	public void quitter()
+	{
+		Debug.Log ("Le jeu va se fermer!");
+		Application.Quit ();
+	}
+
+	public void commentJouer()
+	{
+		
+	}
+
+	public void touches()
+	{
+		
+	}
+
+	public void jouer()
+	{
+		
+	}
+}
