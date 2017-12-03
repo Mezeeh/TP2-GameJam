e9c901a15338f811c73e82fbac9f77d954ce17c1
Sedrick Levesque <sedrickidd@hotmail.com>
2017-12-03 11:50:05 -0500

    Ajout de movement adversaire

---
+++ TP2-GameJam/Assets/scripts/MovementAdversaire.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAdversaire : MonoBehaviour {
+	
+	public float vitesse = 5;
+	private PathFinding pathfinder;
+	private Grille grille;
+	private Vector3 positionCible;
+	private Noeud noeudDepart, noeudArrivee;
+	private bool aggresser; 
+	// Use this for initialization
+	void Start () {
+		aggresser = false;
+
+		pathfinder = GameObject.Find ("A*").GetComponent<PathFinding> ();
+		grille = GameObject.Find ("A*").GetComponent<Grille> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		
+	}
+	private void SetRoute(){
+		noeudDepart = grille.noeudVsPoint (transform.position);
+	}
+}
