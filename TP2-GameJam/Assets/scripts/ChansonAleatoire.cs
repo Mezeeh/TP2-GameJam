bc8935e938975f258cfc18e2e935b2a2ebb882d5
Mezeeh <mezeeh@hotmail.com>
2017-12-03 19:09:23 -0500

    Gestion son termine
    Son aleatoire en jeu et bruit de sifflet au debut dune round

---
+++ TP2-GameJam/Assets/scripts/ChansonAleatoire.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChansonAleatoire : MonoBehaviour {
+
+	public AudioClip[] chansons;
+	private AudioSource source;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		source = this.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(source != null && !source.isPlaying)
+		{
+			source.PlayOneShot (randomChanson());
+		}
+	}
+
+	AudioClip randomChanson()
+	{
+		return chansons[Random.Range(0, chansons.Length)];
+	}
+}
